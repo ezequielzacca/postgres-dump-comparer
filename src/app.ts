82cabc281afc0f34592a769030562b3fbf7c2a62
Ezequiel Zacca <ezequielzacca@MacBook-Air-de-Ezequiel.local>
2021-01-26 09:39:58 -0300

    improvements

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,59 @@
-import { backupPostgresFunctions } from "./commands"
+import { backupPostgresFunctions, backupPostgresTables } from "./commands"
 import { config } from "dotenv"
+import ora from "ora"
+import inquirer from "inquirer"
 config()
 const executeProgram = async () => {
-  console.log()
-  await backupPostgresFunctions(
+  const answer = await inquirer.prompt([{
+    name:"choices",
+    type: "checkbox",
+    message: "Select the objects that you want to dump",
+    choices: [
+      {
+        name: "Functions",
+        checked: true,
+      },
+      {
+        name: "Tables",
+        checked: false
+      }
+    ], validate: function (answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one.';
+      }
+      return true;
+    },
+  }])
+  const promisesToResolve: Promise<any>[] = []
+  const spinner = ora('Dumping selected objects...').start()
+  const dumpProdFunctions = backupPostgresFunctions(
     process.env.CONNECTION_STRING_PROD as string,
     "prod"
   )
-  await backupPostgresFunctions(
+
+  const dumpDevFunctions = backupPostgresFunctions(
     process.env.CONNECTION_STRING_DEV as string,
     "dev"
   )
+
+  const dumpProdTables = backupPostgresTables(
+    process.env.CONNECTION_STRING_PROD as string,
+    "prod"
+  )
+
+  const dumpDevTables = backupPostgresTables(
+    process.env.CONNECTION_STRING_DEV as string,
+    "dev"
+  )
+  if(answer.choices.includes("Functions")){
+    promisesToResolve.push(dumpProdFunctions)
+    promisesToResolve.push(dumpDevFunctions)
+  }
+  if(answer.choices.includes("Tables")){
+    promisesToResolve.push(dumpProdTables)
+    promisesToResolve.push(dumpDevTables)
+  }
+  await Promise.all(promisesToResolve)
+  spinner.stop()
 }
 executeProgram().then(() => console.log("finished")).catch(console.log);
--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,9 @@
-import { writeFileSync, writeSync } from "fs";
+import { exec, spawn } from "child_process";
+import { writeFileSync } from "fs";
 import path from "path"
 import pg from "pg"
+import { Promesify } from "js-promesify"
+import { parse } from "pg-connection-string"
 export const backupPostgresFunctions = async (
   connectionString: string,
   filename: string
@@ -20,9 +23,66 @@ export const backupPostgresFunctions = async (
     await client.connect()
     const { rows } = await client.query<{ function_text: string }>(query)
     const text = rows.map(row => row.function_text).join("\r\n\r\n")
-    writeFileSync(path.join(process.cwd(), "output", `${filename}.sql`), text)
+    writeFileSync(path.join(process.cwd(), "output", `functions-${filename}.sql`), text)
   } catch (e) {
     console.log("Error: ", e)
   }
 
 };
+
+const wait = (time: number) => new Promise((resolve) => {
+  setTimeout(resolve, time * 1000)
+})
+
+export const backupPostgresTables = async (
+  connectionString: string,
+  filename: string
+) => {
+  try {
+    const query = `
+    SELECT table_name FROM information_schema.tables WHERE table_schema='public';
+    `
+    const client = new pg.Client({
+      connectionString
+    })
+    await client.connect()
+    const connectionParams = parse(connectionString)
+
+    const { rows } = await client.query<{ table_name: string }>(query)
+    const tables = rows.map(row => row.table_name)
+    const destinationFilePath = path.join(process.cwd(), "output", `tables-${filename}.sql`)
+    const extraEnv ={
+      "PGPASSWORD": connectionParams.password!,
+      "PGSSLMODE": "require"
+    }
+    
+    const args = [
+      connectionString,
+      ...tables.map(t => `-t '\"${t}\"'`),
+      `--schema-only`,
+      `-f ${destinationFilePath}`
+    ]
+    
+    await executeCommand("pg_dump", args)
+  } catch (e) {
+    console.log("Error: ", e)
+  }
+
+};
+
+export const executeCommand = (
+  command: string,
+  args: string[]
+): Promise<void> => {
+  return new Promise<void>((resolve, reject) => {
+    const spawnedCmd = exec(`${command} ${args.join(" ")}`);
+    
+        //spawnedCmd.stdin.write(`${text}\r\n`);
+        spawnedCmd.on("exit", () => {
+          resolve()
+
+    });
+  });
+};
+
+
